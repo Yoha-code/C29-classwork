793f171f87e5b68c96d222c22237d618bf1f36cf
Yoha-code <85884213+Yoha-code@users.noreply.github.com>
2021-10-04 18:46:10 +0530

    Cut the rope classwork C29

---
+++ sketch.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var rope;
+var fruit, fruit_con;
+
+function setup() 
+{
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(200,680,600,20);
+
+  rope = new Rope(7, {x: 250, y: 25});
+
+  fruit = Matter.Bodies.circle(250, 50, 20);
+  Matter.Composite.add(rope.body, fruit);
+
+  fruit_con = new link(rope, fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  background(51);
+  ground.show();
+  rope.show();
+  ellipse(fruit.position.x, fruit.position.y, 20, 20);
+  
+  Engine.update(engine);
+  
+}
